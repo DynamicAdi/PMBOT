a3f3e9dad708bb93c8f3f41dc80b89581396ae0d
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-21 21:30:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os 
+
+class config(object):
+  API_ID = 2443790
+  API_HASH = "b00e27babc25c281d2ec18242b5b6760"
+  TOKEN = "1905648151:AAH4DNh2VneV_VQ3v2MTc0dEbZbVwFrz8dQ"
+# NO EDITS 
+  PIC = "https://telegra.ph/file/124965f314c03a415f1c4.jpg"
+  OWNER_USERNAME = "Alone_loverboy"
+  OWNER_ID = 1258905497 
+  DB_URI = os.environ.get("DATABASE_URL", None)
+
+# My account configuration
+# Don't use mine and create 🙂enemy
