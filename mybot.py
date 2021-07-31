7bb642ab911eb48f6b3e08ff306057282ea6c523
(◍•ᴗ•◍) <79841865+loverboyXD@users.noreply.github.com>
2021-08-01 00:56:32 +0530

    😊

--- mybot.py
+++ mybot.py
@@ -2,15 +2,16 @@ import os
 import re
 import html
 import time 
+from sql.users_sql import get_user_id
 from datetime import datetime
 ### imports == Telethon 
 from telethon import Button 
 from telethon import events 
 from telethon import TelegramClient 
-### imports == Pyrogram 
-from pyrogram import Client 
-from pyrogram import filters
-from pyrogram.types import Message 
+from telethon.utils import pack_bot_file_id
+
+
+
 # config # 
 from config import config
 ## <--- Import finish --->
@@ -237,6 +238,42 @@ async def one_new_mssg(event):
    
 # {Pyrogram} #
 ## [<---outgoing--->] ##
+@pm.on(events.NewMessage(func=lambda e: e.is_private))
+
+async def on_out_mssg(event):
+
+    to_send = await event.get_reply_message()
+
+    if to_send is None:
+
+        return
+
+    to_send.id
+
+    send_mssg = event.raw_text
+
+    who = event.sender_id
+
+    user_id, reply_message_id = get_user_id(to_send.id)
+
+    if who == OWNER_ID:
+
+        if send_mssg.startswith("/"):
+
+            return
+
+        if event.text is not None and event.media:
+
+            # if sending media
+
+            bot_api_file_id = pack_bot_file_id(event.media)
+
+            await pm.send_file(user_id, file=bot_api_file_id, caption=event.text, reply_to=reply_message_id)
+
+        else:
+
+            await pm.send_message(user_id, send_mssg, reply_to=reply_message_id,)
+
 
 # below is startup
 
