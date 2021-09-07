b6f2e79bc9d9d1df181eb9aa0c0fbb7889d7a21d
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-09-07 23:31:41 +0530

    hmm

--- mybot.py
+++ mybot.py
@@ -224,7 +224,48 @@ async def one_new_mssg(event):
         event.chat_id
         await event.forward_to(OWNER_ID)
         
-     
+# OUTGOING 
+from telethon import events
+
+from telethon.utils import pack_bot_file_id
+
+OWNER_ID = 1258905497
+
+@tgbot.on(events.NewMessage(func=lambda e: e.is_private))
+
+async def on_out_mssg(event):
+
+    to_send = await event.get_reply_message()
+
+    if to_send is None:
+
+        return
+
+    sender = event.chat_id
+
+    send_mssg = event.raw_text
+
+    who = event.chat_id
+
+    user_id, reply_message_id = sender
+
+    if who == OWNER_ID:
+
+        if send_mssg.startswith("/"):
+
+            return
+
+        if event.text is not None and event.media:
+
+            # if sending media
+
+            bot_api_file_id = pack_bot_file_id(event.media)
+
+            await tgbot.send_file(user_id, file=bot_api_file_id, caption=event.text, reply_to=reply_message_id)
+
+        else:
+
+            await tgbot.send_message(user_id, send_mssg, reply_to=reply_message_id,)
 
 # below is startup
 
